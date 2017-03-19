7a0121032283a13a0648d8455ad17e92569ac8f7
Yin Chen <yinchen@freewheel.tv>
2017-03-19 19:50:30 +0800

    Add crontab management

--- Hors.go
+++ Hors.go
@@ -5,6 +5,9 @@ import (
     "net/http"
     "log"
     "github.com/gorilla/mux"
+    "github.com/gorhill/cronexpr"
+    "time"
+    "encoding/json"
 )
 
 func Demo(w http.ResponseWriter, r *http.Request) {
@@ -17,9 +20,37 @@ func PathDemo(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Path: %v \n", vars["file_path"])
 }
 
+type CRON_JOB struct{
+    Cron        string `json:"cron"`
+    ExecuteFile string `json:"execute_file"`
+    Args        string `json:"args"`
+}
+
+func CronRegDemo(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    var cron CRON_JOB
+    switch r.Method {
+    case "POST":
+        io.WriteString(w, "Gorilla!\n")
+        decoder := json.NewDecoder(r.Body)
+        decoder.Decode(&cron)
+        cron.ExecuteFile = vars["file_path"]
+        break
+    case "GET":
+        cron = CRON_JOB{Cron:"* */2 * * *", ExecuteFile:"demo_file.sh", Args: "arg1 arg2"}
+        break
+    }
+    expr := cronexpr.MustParse(cron.Cron)
+    nextTime := expr.Next(time.Now())
+    fmt.Fprintf(w, "Path: %v \t Cron: %v \t Next Time: %v\n",
+    vars["file_path"], cron.Cron, nextTime)
+}
+
 func main() {
     r := mux.NewRouter()
-    r.HandleFunc("/exec/{file_path:.*}", Demo).Methods("GET")
+    r.HandleFunc("/refer/{file_path:.*}", Demo).Methods("GET")
     r.HandleFunc("/exec/{file_path:.*}", PathDemo).Methods("POST")
+    r.HandleFunc("/cron/{file_path:.*}", CronRegDemo)
+    r.HandleFunc("/cron/{file_path:.*}", CronRegDemo).Methods("POST")
     log.Fatal(http.ListenAndServe(":8000", r))
 }
